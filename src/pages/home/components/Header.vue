3849ed908fcc0bcff9ab91670ba7ff8022db089a
jinyajuan <2633579745@qq.com>
2019-02-27 17:51:31 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/默认主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+.header{
+  display: flex;
+  line-height: .86rem;
+  background-color: $bgColor;
+  color: #fff;
+}
+.header-left{
+  width: .64rem;
+  float: left;
+}
+.back-iconfont{
+  text-align: center;
+  font-size: .4rem;
+}
+.header-input{
+  flex: 1;
+  height: .64rem;
+  line-height: .64rem;
+  background-color: #fff;
+  border-radius: .1rem;
+  margin-top: .12rem;
+  margin-left: .2rem;
+  padding-left: .2rem;
+  color: #ccc;
+}
+.header-right{
+  width: 1.24rem;
+  text-align: center;
+}
+</style>
