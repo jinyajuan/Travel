45bf886809f18b391b766d63db4b41f70f67de79
jinyajuan <2633579745@qq.com>
2019-02-28 22:50:25 +0800

    city list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<!--<style scoped>-->
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+.list{
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  right: 0;
+  top: 1.58rem;
+  bottom: 0;
+  /*background-color: red;*/
+  width: .4rem;
+}
+  .item{
+    line-height: .4rem;
+    color: $bgColor;
+  }
+</style>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -13,14 +13,14 @@ export default {
 <style scoped lang="stylus">
   @import '~styles/varibles.styl'
   .search{
-    height: .72rem;
+    height: .6rem;
     padding: .1rem;
     background-color: $bgColor;
   }
   .search-input{
     box-sizing: border-box;
     width: 100%;
-    height: .62rem;
+    height: .52rem;
     line-height: .62rem;
     text-align: center;
     border-radius: .06rem;
