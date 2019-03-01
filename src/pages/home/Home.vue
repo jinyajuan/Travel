e93db10399528eaac506603ee4ed271ed8a792e2
jinyajuan <2633579745@qq.com>
2019-03-01 18:42:30 +0800

    city page finish

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,8 @@ import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
 import axios from 'axios'
+import { mapState } from 'vuex'
+
 export default {
   name: 'Home',
   components: {
@@ -26,6 +28,7 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
@@ -34,7 +37,7 @@ export default {
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -50,8 +53,20 @@ export default {
       console.log(res)
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   mounted () {
+    this.lastCity = this.city
+    // 对上次城市进行一个保存
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      // 当本次城市与之前城市不一致的时候，再次发送请求
+      this.getHomeInfo()
+    }
   }
 }
 </script>
